ed43a880a99cfbfb135a73f6839bf5b87d03d559
G00401655@atu.ie <G00401655@atu.ie>
2023-11-08 14:52:20 +0000

    first push p2

---
+++ src/main/java/ie/atu/wk7exam2/Wk7Exam2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.wk7exam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk7Exam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wk7Exam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/wk7exam2/Wk7Exam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.wk7exam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk7Exam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
